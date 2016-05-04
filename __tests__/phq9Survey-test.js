0a50edf8f6f0f6527797d925196dea6067375581
Skylar Panuska <skylar.panuska@gmail.com>
2016-05-03 21:37:19 -0400

    adds working test

---
+++ __tests__/phq9Survey-test.js
@@ -0,0 +1,31 @@
+jest.dontMock('../phq9Survey.js');
+
+var React = require('react/addons');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var QuestionCard = require('../phq9Survey.js').QuestionCard;
+
+describe('Question Card', function() {
+
+	var question = {
+		content: 'Little interest or pleasure in doing things', 
+		topic: 'pleasure'
+	};
+
+	describe('the display of the question content', function() {
+
+		it('should display the text of the question.', function() {
+
+                  var renderer = TestUtils.createRenderer();
+                  renderer.render( <QuestionCard question={question.content} />);
+                  var output = renderer.getRenderOutput();
+                  console.log(output);
+
+
+                  expect(output.props.children).toEqual(question.content);
+
+		})
+
+	});
+
+})
--- phq9Survey.js
+++ phq9Survey.js
@@ -175,34 +175,5 @@ var SurveyContainer = React.createClass({
 });
 
 
-var QUESTIONS = [
-	{ content: 'Little interest or pleasure in doing things', topic: 'pleasure'},
-	{ content: 'Feeling down, depressed, or hopeless', topic: 'feeling down'},
-	{ content: 'Trouble falling or staying asleep, or sleeping too much', topic: 'sleep'},
-	{ content: 'Feeling tired or having little energy', topic: 'energy'},
-	{ content: 'Poor appetite or overeating', topic: 'appetite'},
-	{ content: 'Feeling bad about yourself - or that you are a failure or have let yourself or your family down', topic: 'self esteem'},
-	{ content: 'Trouble concentrating on things, such as reading the newspaper or watching television', topic: 'concentration'},
-	{ content: 'Moving or speaking so slowly that other people could have noticed? Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual', topic: 'movement'},
-	{ content: 'Thoughts that you would be better off dead, or of hurting yourself in some way', topic: 'suicidal ideation'}
-];
-
-
-var RESPONSES = [
-	{content: 'Not at all', points: 0},
-	{content: 'Several days', points: 1},
-	{content: 'More than half the days', points: 2},
-	{content: 'Nearly every day', points: 3}
-]
-
-var RESOURCES = [
-	{id: 1, firstName: 'Jaime', lastName: 'Marx', street: '123 1st St.', city: 'New York', state: 'NY', phone: '111-111-1111', specialty: 'Depression'},
-	{id: 2, firstName: 'Rae', lastName: 'Specie', street: '456 2nd St.', city: 'New York', state: 'NY', phone: '222-222-2222', specialty: 'Depression'},
-	{id: 3, firstName: 'First', lastName: 'Last', street: '789 3rd Ave.', city: 'New York', state: 'NY', phone: '333-333-3333', specialty: 'Depression'}
-]
-
-
-ReactDOM.render(
-  <SurveyContainer questions={QUESTIONS} responses={RESPONSES} resources={RESOURCES} />,
-  document.getElementById('container')
-);
+module.exports.QuestionCard = QuestionCard;
+module.exports.SurveyContainer = SurveyContainer;
